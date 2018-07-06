e7f8e9e407d96cca0b02b6152e889b0a7efc80ce
SopQ <sopcc@qq.com>
2018-07-06 10:52:42 +0800

    update src/ItemDoc.Services/ItemDoc.Services.csproj

--- src/ItemDoc.Web/Controllers/HomeController.cs
+++ src/ItemDoc.Web/Controllers/HomeController.cs
@@ -1,104 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Security.Claims;
-using System.Text;
-using System.Web;
-using System.Web.Mvc;
-using ItemDoc.Core.Auth;
+﻿using ItemDoc.Core.Auth;
 using ItemDoc.Services.Mapping;
-using ItemDoc.Services.Model;
 using ItemDoc.Services.Servers;
 using ItemDoc.Services.ViewModel;
-using Microsoft.AspNet.Identity;
-using Microsoft.Owin.Security;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace ItemDoc.Web.Controllers
 {
-  public class HomeController : Controller
-  {
-    private IAuthenticationService _authentication;
-    private IUsersService _usersService;
-    private IPostService _postService;
-
-    public HomeController(
-      IUsersService usersService,
-      IAuthenticationService authentication, 
-      IPostService postService)
+    public class HomeController : Controller
     {
-      _authentication = authentication;
-      _postService = postService;
-      _usersService = usersService;
+        private IAuthenticationService _authentication;
+        private IUsersService _usersService;
+        private IPostService _postService;
+
+        public HomeController(
+          IUsersService usersService,
+          IAuthenticationService authentication,
+          IPostService postService)
+        {
+            _authentication = authentication;
+            _postService = postService;
+            _usersService = usersService;
+        }
+
+        public ActionResult Index()
+        {
+            var list = _postService.GetAll().MapToList<PostViewModel>().ToList();
+            ViewData["postView"] = list;
+            return View();
+        }
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+
+
+        }
+
+
+        public ActionResult Question()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region 展示图片
+        public ActionResult ShowImage()
+        {
+            //var imageList = db.ImageServerInfo.Where<ImageServerInfo>(c => c.FlgUsable == true).ToList();
+            //ViewData["imageList"] = imageList;
+            return View();
+        }
+        #endregion
+
+
     }
-
-    public ActionResult Index()
-    {
-      var list = _postService.GetAll().MapToList<PostViewModel>().ToList();
-      ViewData["postView"] = list;
-      return View();
-    }
-
-
-
-   
- public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
-
-      return View();
-    }
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
-
-
-
-    }
-
-
-  public ActionResult Question()
-    {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    #region 展示图片
-    public ActionResult ShowImage()
-    {
-      //var imageList = db.ImageServerInfo.Where<ImageServerInfo>(c => c.FlgUsable == true).ToList();
-      //ViewData["imageList"] = imageList;
-      return View();
-    }
-    #endregion
-  
-   
-  }
 }
