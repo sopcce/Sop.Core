ceec95edfb176f7af74cbd9ccec189dea3325410
guojiaqiu <sopcce@qq.com>
2019-01-01 19:26:51 +0800

    1111

--- src/ItemDoc.Web/assets/lib/sopcce.com/main.js
+++ src/ItemDoc.Web/assets/lib/sopcce.com/main.js
@@ -6,54 +6,7 @@ import axios from 'axios';
 Vue.prototype.axios = axios;
 
 Vue.use(ElementUI, { locale });
+ 
 
-new Vue({
-    el: '#app',
-    data: function () {
-        return { visible: false }
-    }
-})
 
-var Ctor1 = Vue.extend({
-    data() {
-        return {
-            dialogImageUrl: '',
-            dialogVisible: false
-        };
-    },
-    methods: {
-        handleRemove(file, fileList) {
-            console.log(file, fileList);
-        },
-        handlePictureCardPreview(file) {
-            debugger
-            console.log(file);
-            this.dialogImageUrl = file.url;
-            this.dialogVisible = true;
-        }
-    }
-});
-new Ctor1().$mount('#app_1');
 
-var Ctor2 = Vue.extend({
-    data() {
-        return {
-            fileList2: [{
-                name: 'food.jpeg',
-                url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-            }, {
-                name: 'food2.jpeg',
-                url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-            }]
-        };
-    },
-    methods: {
-        handleRemove(file, fileList) {
-            console.log(file, fileList);
-        },
-        handlePreview(file) {
-            console.log(file);
-        }
-    }
-});
-new Ctor2().$mount('#app_2');
