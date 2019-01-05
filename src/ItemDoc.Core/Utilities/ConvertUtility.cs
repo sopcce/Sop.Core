755ba31738a7924396b98baa230ab3075ee8b980
guojiaqiu <sopcce@qq.com>
2019-01-05 23:22:59 +0800

    222

---
+++ src/ItemDoc.Core/Utilities/ConvertUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemDoc.Core.Utilities
+{
+    public static class ConvertUtility
+    {
+        public static byte[] StreamToBytes(Stream stream)
+        {
+            byte[] buffer = new byte[stream.Length];
+            stream.Read(buffer, 0, buffer.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            return buffer;
+        }
+    }
+}
