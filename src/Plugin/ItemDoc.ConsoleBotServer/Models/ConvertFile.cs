e965390d47f2ba676a9b3cae5fdf24a04c658745
guojiaqiu <sopcce@qq.com>
2019-02-26 17:38:44 +0800

    data

---
+++ src/Plugin/ItemDoc.ConsoleBotServer/Models/ConvertFile.cs
@@ -0,0 +1,19 @@
+﻿using ItemDoc.Services.Model;
+
+namespace ItemDoc.ConsoleBotServer.Models
+{
+    public class ConvertFile
+    {
+        public AttachmentInfo attachmentInfo { get; set; }
+
+        public string FromPath { get; set; }
+
+        public string ToPath { get; set; }
+
+        public string FileName { get; set; } 
+      
+        public string Extension { get;  set; }
+    }
+
+
+}
