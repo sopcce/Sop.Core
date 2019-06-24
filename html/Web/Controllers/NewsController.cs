277ac3a8697e761ccc4493399d7e10c8794aa1e0
guojiaqiu <sopcce@qq.com>
2019-06-24 11:40:49 +0800

    增加以类似头条的模板

---
+++ html/Web/Controllers/NewsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult _ugcBox()
+        {
+            return View();
+        }
+        public ActionResult _CssJs()
+        {
+            return View();
+        }
+
+
+        public ActionResult _MenuNews()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+        // GET: News/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // GET: News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: News/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: News/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: News/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: News/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ html/Web/css2js/index.js
@@ -0,0 +1,3 @@
+//function(t) {
+//    if (t.target === t.currentTarget || t.timeStamp >= o || t.timeStamp <= 0 || t.target.ownerDocument !== document) return i.apply(this, arguments)
+//};
