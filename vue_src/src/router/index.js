e0879e67cd94db9d278085900f929dc704030343
guojiaqiu <sopcce@qq.com>
2020-04-07 19:36:51 +0800

    add

--- vue_src/src/router/index.js
+++ vue_src/src/router/index.js
@@ -7,9 +7,7 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
+
 import nestedRouter from './modules/nested'
 
 /**
@@ -157,7 +155,6 @@ export const asyncRoutes = [
         meta: {
           title: 'directivePermission',
           icon: 'size'
-
           // 如果不设置角色，意味着:此页面不需要权限
         }
       },
@@ -183,6 +180,234 @@ export const asyncRoutes = [
       icon: 'example'
     },
     children: [
+      // table
+      {
+        path: '/table',
+        component: Layout,
+        redirect: '/table/complex-table',
+        name: 'Table',
+        meta: {
+          title: 'Table',
+          icon: 'table'
+        },
+        children: [
+          {
+            path: 'dynamic-table',
+            component: () => import('@/views/table/dynamic-table/index'),
+            name: 'DynamicTable',
+            meta: { title: 'dynamicTable' }
+          },
+          {
+            path: 'drag-table',
+            component: () => import('@/views/table/drag-table'),
+            name: 'DragTable',
+            meta: { title: 'dragTable' }
+          },
+          {
+            path: 'inline-edit-table',
+            component: () => import('@/views/table/inline-edit-table'),
+            name: 'InlineEditTable',
+            meta: { title: 'inlineEditTable' }
+          },
+          {
+            path: 'complex-table',
+            component: () => import('@/views/table/complex-table'),
+            name: 'ComplexTable',
+            meta: { title: 'complexTable' }
+          }
+        ]
+      },
+      // error
+      {
+        path: '/error',
+        component: Layout,
+        redirect: 'noRedirect',
+        name: 'ErrorPages',
+        meta: {
+          title: 'errorPages',
+          icon: '404'
+        },
+        children: [
+          {
+            path: '/error/401',
+            component: () => import('@/views/error-page/401'),
+            name: 'Page401',
+            meta: { title: 'page401', noCache: true }
+          },
+          {
+            path: '/error/404',
+            component: () => import('@/views/error-page/404'),
+            name: 'Page404',
+            meta: { title: 'page404', noCache: true }
+          }
+        ]
+      },
+      // excel
+      {
+        path: '/excel',
+        component: Layout,
+        redirect: '/excel/export-excel',
+        name: 'Excel',
+        meta: {
+          title: 'excel',
+          icon: 'excel'
+        },
+        children: [
+          {
+            path: 'export-excel',
+            component: () => import('@/views/excel/export-excel'),
+            name: 'ExportExcel',
+            meta: { title: 'exportExcel' }
+          },
+          {
+            path: 'export-selected-excel',
+            component: () => import('@/views/excel/select-excel'),
+            name: 'SelectExcel',
+            meta: { title: 'selectExcel' }
+          },
+          {
+            path: 'export-merge-header',
+            component: () => import('@/views/excel/merge-header'),
+            name: 'MergeHeader',
+            meta: { title: 'mergeHeader' }
+          },
+          {
+            path: 'upload-excel',
+            component: () => import('@/views/excel/upload-excel'),
+            name: 'UploadExcel',
+            meta: { title: 'uploadExcel' }
+          }
+        ]
+      },
+      // charts
+      {
+        path: '/charts',
+        component: Layout,
+        redirect: 'noRedirect',
+        name: 'Charts',
+        meta: {
+          title: 'charts',
+          icon: 'chart'
+        },
+        children: [
+          {
+            path: 'keyboard',
+            component: () => import('@/views/charts/keyboard'),
+            name: 'KeyboardChart',
+            meta: { title: 'keyboardChart', noCache: true }
+          },
+          {
+            path: 'line',
+            component: () => import('@/views/charts/line'),
+            name: 'LineChart',
+            meta: { title: 'lineChart', noCache: true }
+          },
+          {
+            path: 'mix-chart',
+            component: () => import('@/views/charts/mix-chart'),
+            name: 'MixChart',
+            meta: { title: 'mixChart', noCache: true }
+          }
+        ]
+      },
+      // components
+      {
+        path: '/components',
+        component: Layout,
+        redirect: 'noRedirect',
+        name: 'ComponentDemo',
+        meta: {
+          title: 'components',
+          icon: 'component'
+        },
+        children: [
+          {
+            path: 'tinymce',
+            component: () => import('@/views/components-demo/tinymce'),
+            name: 'TinymceDemo',
+            meta: { title: 'tinymce' }
+          },
+          {
+            path: 'markdown',
+            component: () => import('@/views/components-demo/markdown'),
+            name: 'MarkdownDemo',
+            meta: { title: 'markdown' }
+          },
+          {
+            path: 'json-editor',
+            component: () => import('@/views/components-demo/json-editor'),
+            name: 'JsonEditorDemo',
+            meta: { title: 'jsonEditor' }
+          },
+          {
+            path: 'split-pane',
+            component: () => import('@/views/components-demo/split-pane'),
+            name: 'SplitpaneDemo',
+            meta: { title: 'splitPane' }
+          },
+          {
+            path: 'avatar-upload',
+            component: () => import('@/views/components-demo/avatar-upload'),
+            name: 'AvatarUploadDemo',
+            meta: { title: 'avatarUpload' }
+          },
+          {
+            path: 'dropzone',
+            component: () => import('@/views/components-demo/dropzone'),
+            name: 'DropzoneDemo',
+            meta: { title: 'dropzone' }
+          },
+          {
+            path: 'sticky',
+            component: () => import('@/views/components-demo/sticky'),
+            name: 'StickyDemo',
+            meta: { title: 'sticky' }
+          },
+          {
+            path: 'count-to',
+            component: () => import('@/views/components-demo/count-to'),
+            name: 'CountToDemo',
+            meta: { title: 'countTo' }
+          },
+          {
+            path: 'mixin',
+            component: () => import('@/views/components-demo/mixin'),
+            name: 'ComponentMixinDemo',
+            meta: { title: 'componentMixin' }
+          },
+          {
+            path: 'back-to-top',
+            component: () => import('@/views/components-demo/back-to-top'),
+            name: 'BackToTopDemo',
+            meta: { title: 'backToTop' }
+          },
+          {
+            path: 'drag-dialog',
+            component: () => import('@/views/components-demo/drag-dialog'),
+            name: 'DragDialogDemo',
+            meta: { title: 'dragDialog' }
+          },
+          {
+            path: 'drag-select',
+            component: () => import('@/views/components-demo/drag-select'),
+            name: 'DragSelectDemo',
+            meta: { title: 'dragSelect' }
+          },
+          {
+            path: 'dnd-list',
+            component: () => import('@/views/components-demo/dnd-list'),
+            name: 'DndListDemo',
+            meta: { title: 'dndList' }
+          },
+          {
+            path: 'drag-kanban',
+            component: () => import('@/views/components-demo/drag-kanban'),
+            name: 'DragKanbanDemo',
+            meta: { title: 'dragKanban' }
+          }
+        ]
+      },
+
       {
         path: 'index',
         component: () => import('@/views/icons/index'),
@@ -217,176 +442,110 @@ export const asyncRoutes = [
         component: () => import('@/views/tab/index'),
         name: 'Tab',
         meta: { title: 'tab', icon: 'tab' }
+      },
+
+      {
+        path: '/error-log',
+        component: Layout,
+        children: [
+          {
+            path: 'log',
+            component: () => import('@/views/error-log/index'),
+            name: 'ErrorLog',
+            meta: { title: 'errorLog', icon: 'bug' }
+          }
+        ]
+      },
+      // zip
+      {
+        path: '/zip',
+        component: Layout,
+        redirect: '/zip/download',
+        alwaysShow: true,
+        name: 'Zip',
+        meta: { title: 'zip', icon: 'zip' },
+        children: [
+          {
+            path: 'download',
+            component: () => import('@/views/zip/index'),
+            name: 'ExportZip',
+            meta: { title: 'exportZip' }
+          }
+        ]
+      },
+
+      {
+        path: '/pdf',
+        component: Layout,
+        redirect: '/pdf/index',
+        children: [
+          {
+            path: 'index',
+            component: () => import('@/views/pdf/index'),
+            name: 'PDF',
+            meta: { title: 'pdf', icon: 'pdf' }
+          }
+        ]
+      },
+      {
+        path: '/pdf/download',
+        component: () => import('@/views/pdf/download'),
+        hidden: true
+      },
+
+      {
+        path: '/theme',
+        component: Layout,
+        children: [
+          {
+            path: 'index',
+            component: () => import('@/views/theme/index'),
+            name: 'Theme',
+            meta: { title: 'theme', icon: 'theme' }
+          }
+        ]
+      },
+
+      {
+        path: '/clipboard',
+        component: Layout,
+        children: [
+          {
+            path: 'index',
+            component: () => import('@/views/clipboard/index'),
+            name: 'ClipboardDemo',
+            meta: { title: 'clipboardDemo', icon: 'clipboard' }
+          }
+        ]
+      },
+
+      {
+        path: '/i18n',
+        component: Layout,
+        children: [
+          {
+            path: 'index',
+            component: () => import('@/views/i18n-demo/index'),
+            name: 'I18n',
+            meta: { title: 'i18n', icon: 'international' }
+          }
+        ]
+      },
+
+      {
+        path: 'external-link',
+        component: Layout,
+        children: [
+          {
+            path: 'https://github.com/PanJiaChen/vue-element-admin',
+            meta: { title: 'externalLink', icon: 'link' }
+          }
+        ]
       }
     ]
   },
   /** 当你的路由图太长时，你可以把它分成小模块 **/
-  componentsRouter,
-  chartsRouter,
   nestedRouter,
-  tableRouter,
-
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'errorPages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '/error/401',
-        component: () => import('@/views/error-page/401'),
-        name: 'Page401',
-        meta: { title: 'page401', noCache: true }
-      },
-      {
-        path: '/error/404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: 'page404', noCache: true }
-      }
-    ]
-  },
-
-  {
-    path: '/error-log',
-    component: Layout,
-    children: [
-      {
-        path: 'log',
-        component: () => import('@/views/error-log/index'),
-        name: 'ErrorLog',
-        meta: { title: 'errorLog', icon: 'bug' }
-      }
-    ]
-  },
-
-  {
-    path: '/excel',
-    component: Layout,
-    redirect: '/excel/export-excel',
-    name: 'Excel',
-    meta: {
-      title: 'excel',
-      icon: 'excel'
-    },
-    children: [
-      {
-        path: 'export-excel',
-        component: () => import('@/views/excel/export-excel'),
-        name: 'ExportExcel',
-        meta: { title: 'exportExcel' }
-      },
-      {
-        path: 'export-selected-excel',
-        component: () => import('@/views/excel/select-excel'),
-        name: 'SelectExcel',
-        meta: { title: 'selectExcel' }
-      },
-      {
-        path: 'export-merge-header',
-        component: () => import('@/views/excel/merge-header'),
-        name: 'MergeHeader',
-        meta: { title: 'mergeHeader' }
-      },
-      {
-        path: 'upload-excel',
-        component: () => import('@/views/excel/upload-excel'),
-        name: 'UploadExcel',
-        meta: { title: 'uploadExcel' }
-      }
-    ]
-  },
-
-  {
-    path: '/zip',
-    component: Layout,
-    redirect: '/zip/download',
-    alwaysShow: true,
-    name: 'Zip',
-    meta: { title: 'zip', icon: 'zip' },
-    children: [
-      {
-        path: 'download',
-        component: () => import('@/views/zip/index'),
-        name: 'ExportZip',
-        meta: { title: 'exportZip' }
-      }
-    ]
-  },
-
-  {
-    path: '/pdf',
-    component: Layout,
-    redirect: '/pdf/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/pdf/index'),
-        name: 'PDF',
-        meta: { title: 'pdf', icon: 'pdf' }
-      }
-    ]
-  },
-  {
-    path: '/pdf/download',
-    component: () => import('@/views/pdf/download'),
-    hidden: true
-  },
-
-  {
-    path: '/theme',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/theme/index'),
-        name: 'Theme',
-        meta: { title: 'theme', icon: 'theme' }
-      }
-    ]
-  },
-
-  {
-    path: '/clipboard',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/clipboard/index'),
-        name: 'ClipboardDemo',
-        meta: { title: 'clipboardDemo', icon: 'clipboard' }
-      }
-    ]
-  },
-
-  {
-    path: '/i18n',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/i18n-demo/index'),
-        name: 'I18n',
-        meta: { title: 'i18n', icon: 'international' }
-      }
-    ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://github.com/PanJiaChen/vue-element-admin',
-        meta: { title: 'externalLink', icon: 'link' }
-      }
-    ]
-  },
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
